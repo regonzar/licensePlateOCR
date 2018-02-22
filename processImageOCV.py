d22ab2223de21c808f4306ccf346c19d210d0616
Tomas <tcastrog10@gmail.com>
2018-02-22 15:29:26 -0300

    bounding boxes filter now working

--- processImageOCV.py
+++ processImageOCV.py
@@ -1,6 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import copy
 np.set_printoptions(threshold=np.inf)
 
 
@@ -29,20 +30,24 @@ def processImage(file='./camion3.png', output='./license3processed.png',
 
     image = firstFilter(image)
     image = secondFilter(image)
+    image = getBoundingBoxes(image)
 
     cv2.imwrite(output, image)
 
 
 def preFilter(img):
 
+    print('Pre-Filter entered')
+
     row_mean = np.mean(img, axis=1)
     col_mean = np.mean(img, axis=0)
+
 
     for i, row in enumerate(row_mean):
         hist, bins = np.histogram(img[i, :], bins=[0, 122, 255])
         hist = np.array(hist).astype(float)
 
-        if (hist[0] / (hist[0] + hist[1]) > 0.2):
+        if (hist[0] / (hist[0] + hist[1]) > 0.1):
             mask_row_upper = i
             break
 
@@ -50,7 +55,7 @@ def preFilter(img):
         hist, bins = np.histogram(img[i, :], bins=[0, 122, 255])
         hist = np.array(hist).astype(float)
 
-        if (hist[0] / (hist[0] + hist[1]) > 0.5):
+        if (hist[0] / (hist[0] + hist[1]) > 0.1):
             mask_row_lower = i
             break
 
@@ -58,7 +63,7 @@ def preFilter(img):
         hist, bins = np.histogram(img[:, i], bins=[0, 122, 255])
         hist = np.array(hist).astype(float)
 
-        if (hist[0] / (hist[0] + hist[1]) > 0.2):
+        if (hist[0] / (hist[0] + hist[1]) > 0.25):
             mask_col_left = i
             break
 
@@ -66,7 +71,7 @@ def preFilter(img):
         hist, bins = np.histogram(img[:, i], bins=[0, 122, 255])
         hist = np.array(hist).astype(float)
 
-        if (hist[0] / (hist[0] + hist[1]) > 0.2):
+        if (hist[0] / (hist[0] + hist[1]) > 0.25):
             mask_col_right = i
             break
 
@@ -91,7 +96,7 @@ def firstFilter(img):
         hist, bins = np.histogram(img[i, :], bins=[0, 122, 255])
         hist = np.array(hist).astype(float)
 
-        if (hist[1] / (hist[0] + hist[1]) > 0.75):
+        if (hist[1] / (hist[0] + hist[1]) > 0.551):
             mask_row_upper = i
             break
 
@@ -99,7 +104,7 @@ def firstFilter(img):
         hist, bins = np.histogram(img[i, :], bins=[0, 122, 255])
         hist = np.array(hist).astype(float)
 
-        if (hist[1] / (hist[0] + hist[1]) > 0.75):
+        if (hist[1] / (hist[0] + hist[1]) > 0.76):
             mask_row_lower = i
             break
 
@@ -107,7 +112,7 @@ def firstFilter(img):
         hist, bins = np.histogram(img[:, i], bins=[0, 122, 255])
         hist = np.array(hist).astype(float)
 
-        if (hist[1] / (hist[0] + hist[1]) > 0.5):
+        if (hist[1] / (hist[0] + hist[1]) > 0.6):
             mask_col_left = i
             break
 
@@ -115,7 +120,7 @@ def firstFilter(img):
         hist, bins = np.histogram(img[:, i], bins=[0, 122, 255])
         hist = np.array(hist).astype(float)
 
-        if (hist[1] / (hist[0] + hist[1]) > 0.5):
+        if (hist[1] / (hist[0] + hist[1]) > 0.7):
             mask_col_right = i
             break
 
@@ -132,7 +137,7 @@ def secondFilter(img):
     row_mean = np.mean(img, axis=1)
 
     for i, mean in enumerate(row_mean):
-        if mean < 250:
+        if mean < 200:
             img[i, :] = 255
         else:
             break
@@ -146,15 +151,49 @@ def secondFilter(img):
     col_mean = np.mean(img, axis=0)
 
     for i, mean in enumerate(col_mean):
-        if mean < 250:
+        if mean < 200:
             img[:, i] = 255
         else:
             break
 
     for i, mean in enumerate(reversed(col_mean)):
-        if mean < 250:
+        if mean < 200:
             img[:, len(col_mean) - i - 1] = 255
         else:
             break
 
+    return img
+
+
+def getBoundingBoxes(img):
+
+    img[0, :] = 255
+    img[-1, :] = 255
+    img[:, 0] = 255
+    img[:, -1] = 255
+
+    image = copy.deepcopy(img)
+
+    image, cnt, h = cv2.findContours(image, cv2.RETR_TREE,
+                                     cv2.CHAIN_APPROX_SIMPLE)
+
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    # img = cv2.drawContours(img, cnt, -1, (0, 255, 0), 3)
+
+    w, h = img.shape[:2]
+    box_mask = np.zeros((w, h), np.int8)
+
+    for c in cnt:
+        x, y, w, h = cv2.boundingRect(c)
+
+        if w*h > 8000 and w*h < 25000:
+            box_mask = cv2.rectangle(box_mask, (x, y), (x+w, y+h), 255, -1)
+
+    inv_img = cv2.bitwise_not(img)
+    masked_data = cv2.bitwise_and(inv_img, inv_img, mask=box_mask)
+    img = cv2.bitwise_not(masked_data)
+
+    plt.subplot()
+    plt.imshow(img)
+    plt.show()
     return img
